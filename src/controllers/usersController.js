5c794de18d962047f3a0506aae06e8c89cd49e3d
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-11-09 14:51:32 -0300

    Add update method | list by id if user is null

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -29,6 +29,9 @@ class UserController {
     try{
       if (id) {
         const user = await User.findOne({_id: id});
+        if (user === null){
+          return res.status(200).send({ message: 'User not exist.'});
+        };
         return res.status(200).send({ user });
       }
       const users = await User.find();
@@ -44,6 +47,17 @@ class UserController {
     try {
       const deletedUser = await User.deleteOne({_id: id});
       return res.status(201).send({ message: 'User has been deleted.'});
+    } catch (error) {
+      console.log({ erro: error.message });
+      return res.status(400).send({ erro: error.message });
+    };
+  };
+
+  async update(req,res) {
+    const { id } = req.params;
+    try {
+      const updatededUser = await User.findByIdAndUpdate({_id: id}, req.body);
+      return res.status(201).send({ message: 'User has been updated.', updatededUser});
     } catch (error) {
       console.log({ erro: error.message });
       return res.status(400).send({ erro: error.message });
