4abe077694ad53a51fceb9d28522fd3e4394125f
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-11-08 23:32:53 -0300

    trycatch blocks

--- src/controllers/groupsController.js
+++ src/controllers/groupsController.js
@@ -2,18 +2,28 @@ const Group = require('../models/Group');
 
 class GroupsController {
   async list(req, res) {
-    const groups = await Group.find();
-    if (groups.length === 0) return res.send({ message: 'There are no groups to show!'});
-    return res.status(200).send({
-      groups
-    });
-  }
+    try {
+      const groups = await Group.find();
+      if (groups.length === 0) return res.send({ message: 'There are no groups to show!'});
+      return res.status(200).send({
+        groups
+      });
+    } catch (error) {
+      console.log({erro: error.message});
+      res.status(400).send({erro: error.message});
+    };
+  };
 
   async register(req,res) {
-    const group = await Group.create(req.body);
-    return res.send(group);
+    try {
+      const group = await Group.create(req.body);
+      return res.send(group);
+    } catch (error) {
+      console.log({erro: error.message});
+      res.status(400).send({erro: error.message});
+    };
+  };
 
-  }
-}
+};
 
 module.exports = new GroupsController();
