80eec2bceebbb55d16da111806475b3b5506d0dd
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-11-08 23:51:09 -0300

    Alter tex from messages, improve id identification

--- src/controllers/publisherTypesController.js
+++ src/controllers/publisherTypesController.js
@@ -11,7 +11,8 @@ class PublisherTypeController {
       console.log({
         erro: error.message
       });
-    }
+      return res.status(400).send({ erro: error.message})
+    };
   };
 
   async register(req,res) {
@@ -22,38 +23,36 @@ class PublisherTypeController {
       console.log({
         erro: error.message
       });
-      return res.status(400).send({
-        message: 'Type not registered'
-      });
-    }
+      return res.status(400).send({ erro: error.message})
+    };
   };
 
   async remove(req,res) {
-    const id = req.params;
+    const { id } = req.params;
     try {
-      const deletedGroup = await PublisherType.deleteOne({_id: id});
-      return res.status(200).send({message: 'Group removed!'});
+      const deletedType = await PublisherType.deleteOne({_id: id});
+      return res.status(200).send({message: 'Publisher type removed!', deletedType});
      } catch (error) {
       console.log({
         erro: error.message
       });
-      return res.status(400).send({ message: 'Group not removed, try again later.'});
-    }
+      return res.status(400).send({ erro: error.message});
+    };
   };
 
   async update(req,res) {
     try {
-      const id = req.params;
-      const updatedGroupData = await PublisherType.replaceOne({_id: id.id}, req.body);
-      const updatedGroup = await PublisherType.findOne({_id: id.id});
-      return res.status(200).send({ updatedGroup, updatedGroupData });
+      const { id } = req.params;
+      const updatedTypeData = await PublisherType.replaceOne({_id: id}, req.body);
+      const updatedType = await PublisherType.findOne({_id: id});
+      return res.status(200).send({ updatedType, updatedTypeData });
     } catch (error) {
       console.log({
         erro: error.message
       });
-      return res.status(400).send({ message: error.message});
-    }
-  }
+      return res.status(400).send({ erro: error.message});
+    };
+  };
 };
 
 module.exports = new PublisherTypeController();
