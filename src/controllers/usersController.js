f3b35895588ede3fdb495cb4962fcbaeb717b39e
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-11-09 01:09:49 -0300

    Add password validation and list method

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const isEmail = require('isemail');
+const passwordValidator = require('../config/password');
 
 class UserController {
 
@@ -9,12 +10,30 @@ class UserController {
       if (!isEmail.validate(email)){
         return res.status(400).send({ alert: 'Email is invalid, enter a valid email.'});
       };
+      if (!passwordValidator.validate(password)){
+        return res.status(400).send({ alert: 'Password is invalid, enter a valid password with 8 or more digits and without spaces.'});
+      }
       const userSave = await User.create(req.body);
       return res.send({ userSave });
     } catch (error) {
       console.log({ erro: error.message });
       return res.status(400).send({ erro: error.message });
     };
+  };
+
+  async list(req,res) {
+    const { id } = req.params;
+    try{
+      if (id) {
+        const user = await User.findOne({_id: id});
+        return res.status(200).send({ user });
+      }
+      const users = await User.find();
+      return res.status(200).send({ users });
+    } catch (error) {
+      console.log({ erro: error.message });
+      return res.status(400).send({ erro: error.message });
+    }
   };
 };
 
