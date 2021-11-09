0188573606b5e3cdff0011e6d2767f7553833445
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-11-08 22:37:45 -0300

    add trycatch blocks

--- src/controllers/publisherTypesController.js
+++ src/controllers/publisherTypesController.js
@@ -1,17 +1,45 @@
 const PublisherType = require('../models/PublisherType');
 
 class PublisherTypeController {
+
   async list(req, res) {
-    const publisherTypes = await PublisherType.find();
-    if (publisherTypes.length === 0) return res.send({ message: 'There are no publisher types to show!'});
-    return res.status(200).send({ publisherTypes });
-  }
+    try {
+      const publisherTypes = await PublisherType.find();
+      if (publisherTypes.length === 0) return res.send({ message: 'There are no publisher types to show!'});
+      return res.status(200).send({ publisherTypes });
+    } catch (error) {
+      console.log({
+        erro: error.message
+      });
+    }
+  };
 
   async register(req,res) {
-    const publisherType = await PublisherType.create(req.body);
-    return res.send(publisherType);
+    try {
+      const publisherType = await PublisherType.create(req.body);
+      return res.send({ publisherType });
+    } catch (error) {
+      console.log({
+        erro: error.message
+      });
+      return res.status(400).send({
+        message: 'Type not registeres'
+      });
+    }
+  };
 
-  }
-}
+  async remove(req,res) {
+    const id = req.params;
+    try {
+      const deletedGroup = await PublisherType.deleteOne({_id: id});
+      return res.status(200).send({message: 'Group removed!'});
+     } catch (error) {
+      console.log({
+        erro: error.message
+      });
+      return res.status(400).send({ message: 'Group not removed, try again later.'});
+    }
+  };
+};
 
 module.exports = new PublisherTypeController();
